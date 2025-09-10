d89140e3305ae6b337c6ad97833e90f725e995d0
Cedrus Dang <dtt161094@gmail.com>
2025-09-10 21:52:25 +0800

    Create admin page with Chat History, SQL files for sql files uploading and add auto start be, fe, all

--- web_app/backend/backend/settings.py
+++ web_app/backend/backend/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# For the stored uploaded files
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core',
 ]
 
 MIDDLEWARE = [
--- web_app/backend/core/models.py
+++ web_app/backend/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class SQLFile(models.Model):
+    # SQL DB logging to store uploaded SQL files
+    id = models.AutoField(primary_key=True)
+    # File send to /backend/media/sql_files/
+    file = models.FileField(upload_to='sql_files/')
+    time = models.DateTimeField(auto_now_add=True)
+
+class ChatHistory(models.Model):
+    # Log of user queries and corresponding SQL queries
+    id = models.AutoField(primary_key=True)
+    time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    agent = models.CharField(max_length=1) # Name as a, b, c, d
+    prompt = models.TextField()
+    response = models.TextField()
+
